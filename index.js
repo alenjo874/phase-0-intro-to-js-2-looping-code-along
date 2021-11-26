893f5c8394b87237aa02e9353ed7c1ee95b99c94
Alen Jo <alenjo874@gmail.com>
2021-11-26 17:27:09 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let wrapGiftsArray = []
+
+function writeCards(names,event){
+  for (let i = 0; i < names.length; i++){
+    wrapGiftsArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+ return wrapGiftsArray
+}
+
+function countDown(startNum){
+
+  while(startNum >= 0){
+
+  console.log(startNum);
+  startNum--;
+
+  }
+
+}
